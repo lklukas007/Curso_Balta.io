0cc5561afe1db73827ea98e0b52acaa5307b1212
Lucas O.Prado <lklukas@outlook.com>
2023-12-20 23:45:44 -0300

    first commit acesso a dados com dapper dotnet csharp e sql server

---
+++ AcessoADadosComDotNetCSharpDapperESQLServer/BaltaDataAcess/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+internal class Program
+{
+    private const string connection_string = "Server=localhost,1433;Database=balta;User ID=sa;Password=inovafarmaI";
+
+    private static void Main(string[] args)
+    {
+        try
+        {
+            using (var connection = new SqlConnection(connection_string))
+            {
+                connection.Open();
+                Console.WriteLine("Conectado com sucesso!");
+                Console.ReadKey();
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao se conectar! - Erro: {ex.Message}");
+        }
+    }
+}
