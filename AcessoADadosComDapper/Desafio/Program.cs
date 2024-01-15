b1170facbd3657af8ee04a3a37b777c56ea758f0
Lucas de Oliveira Prado <lucas.oliveira@inovafarma.com.br>
2024-01-15 08:34:28 -0300

    fix: cria solution para projeto que estava sem

--- AcessoADadosComDapper/Desafio/Program.cs
+++ AcessoADadosComDapper/Desafio/Program.cs
@@ -6,7 +6,7 @@ namespace Blog
 {
     class Program
     {
-        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+        private const string CONNECTION_STRING = @"Server=localhost,1435;Database=Blog;User ID=sa;Password=inovafarmaI";
 
         static void Main(string[] args)
         {
